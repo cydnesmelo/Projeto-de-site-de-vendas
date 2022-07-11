2d1aceccd107ff64c676ff21e7ec74b212435838
Cydnês Melo <78417597+cydnesmelo@users.noreply.github.com>
2022-07-11 11:47:02 -0300

    Add files via upload
    Projeto em construção

---
+++ estudoCss/script.js
@@ -0,0 +1,15 @@
+// function enviar() {
+//   let env = document.querySelector('.texto').value
+//   if (env != '') {
+//     alert(`Parabéns ${env} Você foi cadastrado(a) com sucesso!`)
+//   }
+// }
+
+// let enviar = () => {
+//   let env = document.querySelector('.texto').value
+//   if (env != '') {
+//     alert(`Parabéns ${env} Você foi cadastrado(a) com sucesso!`)
+//   }
+// }
+
+console.log(require('path'))
