c94d18e784c1d851ed533f07c6a4fbadee931c14
Cydnês Melo <78417597+cydnesmelo@users.noreply.github.com>
2022-07-12 14:16:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// function enviar() {
+//   let env = document.querySelector('.texto').value
+//   if (env != '') {
+//     alert(`Parabéns ${env} Você foi cadastrado(a) com sucesso!`)
+//   }
+// }
+
+// let enviar = () => {
+//   let env = document.querySelector('.texto').value
+//   if (env != '') {
+//     alert(`Parabéns ${env} Você foi cadastrado(a) com sucesso!`)
+//   }
+// }
+
+function pedirOrcamento() {
+  let info = prompt(
+    'Digite seu número de contato que entraremos em contato o mais breve possível'
+  )
+  alert(`Parabéns ${info} Você foi cadastrado(a) com sucesso!`)
+}
